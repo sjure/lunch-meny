b65ddbf8ac67bb3795b5f96f25cac81c9534e786
Sjur Espedal <sjur.espedal@soderbergpartnerswealth.no>
2024-08-09 14:08:58 +0200

    add random loaders

--- src/loader.tsx
+++ src/loader.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import Lottie from 'react-lottie'
 import BeerLoaderJson from '../assets/json-gifs/beer-loader.json'
+import RandomFoodSlider from '../assets/json-gifs/random-food-slider.json'
+import SaladDrink from '../assets/json-gifs/salad-drink.json'
+import FoodMixin from '../assets/json-gifs/food-mixin.json'
+import Hotdog from '../assets/json-gifs/hotdog.json'
 
-const loaderJsons = [BeerLoaderJson]
+const loaderJsons = [BeerLoaderJson, RandomFoodSlider, SaladDrink, FoodMixin, Hotdog]
 
 export const Loader: React.FC = () => {
     const randomLoaderJson = loaderJsons[Math.floor(Math.random() * loaderJsons.length)]
