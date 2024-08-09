e5ffe898fdee08196eeded2f388a4edd4bbef069
Sjur Espedal <sjur.espedal@soderbergpartnerswealth.no>
2024-08-09 14:01:26 +0200

    Select random loader from the list

--- src/loader.tsx
+++ src/loader.tsx
@@ -2,14 +2,18 @@ import React from 'react'
 import Lottie from 'react-lottie'
 import BeerLoaderJson from '../assets/json-gifs/beer-loader.json'
 
+const loaderJsons = [BeerLoaderJson]
+
 export const Loader: React.FC = () => {
+    const randomLoaderJson = loaderJsons[Math.floor(Math.random() * loaderJsons.length)]
+
     return (
         <div>
             <Lottie
                 options={{
                     loop: true,
                     autoplay: true,
-                    animationData: BeerLoaderJson,
+                    animationData: randomLoaderJson,
                 }}
                 height={200}
                 width={200}
